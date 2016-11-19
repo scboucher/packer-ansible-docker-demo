a497c20646a7f4255fd29c70c79fcabd262c3823
Samuel Boucher <boucher.samuel.c@gmail.com>
2016-11-19 11:14:10 -0500

    Decouverte de count ...

--- teamcity-agent.tf
+++ teamcity-agent.tf
@@ -1,40 +1,29 @@
-provider "docker" {
-}
+provider "docker" {}
 
 # Create a container
-resource "docker_container" "agent1" {
-    image = "${docker_image.tc.name}"
-    name = "teamcity-agent-1"
-    command = ["/run-services.sh"]
-    env = ["SERVER_URL=http://${docker_container.server.ip_address}:8111"]
-}
-resource "docker_container" "agent2" {
-    image = "${docker_image.tc.name}"
-    name = "teamcity-agent-2"
-    command = ["/run-services.sh"]
-    env = ["SERVER_URL=http://${docker_container.server.ip_address}:8111"]
-}
-resource "docker_container" "agent3" {
-    image = "${docker_image.tc.name}"
-    name = "teamcity-agent-3"
-    command = ["/run-services.sh"]
-    env = ["SERVER_URL=http://${docker_container.server.ip_address}:8111"]
+resource "docker_container" "agent" {
+  image   = "${docker_image.tc.name}"
+  name    = "teamcity-agent-${ count.index + 1 }"
+  count   = 3
+  command = ["/run-services.sh"]
+  env     = ["SERVER_URL=http://${docker_container.server.ip_address}:8111"]
 }
 
 resource "docker_container" "server" {
-    image = "${docker_image.tc-server.latest}"
-    name = "teamcity-server"
-    hostname ="teamcity"
-    ports {
-        internal = 8111
-        external = 8111
-    }
-}
+  image    = "${docker_image.tc-server.latest}"
+  name     = "teamcity-server"
+  hostname = "teamcity"
 
+  ports {
+    internal = 8111
+    external = 8111
+  }
+}
 
 resource "docker_image" "tc" {
-    name = "scboucher/packer-ansible-demo:0.1"
+  name = "scboucher/packer-ansible-demo:0.1"
 }
+
 resource "docker_image" "tc-server" {
-    name = "jetbrains/teamcity-server"
+  name = "jetbrains/teamcity-server"
 }
